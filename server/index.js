c707ed180e2f5c64b833b3fc99be62a8114e9399
arshgill8 <arsh95g@gmail.com>
2024-01-26 01:32:39 -0500

    bug cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://easy-lease-frontend.vercel.app"],
+    origin: "https://easy-lease-frontend.vercel.app",
     methods: ["POST"],
     credentials: true,
   })
