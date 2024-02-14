bcbecad999f81fa744576bbad9d81f0f91d07283
arshgill8 <arsh95g@gmail.com>
2024-02-14 17:44:04 -0500

    bugs

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@ import App from "./App";
 import "./main.css";
 import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
+// if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
